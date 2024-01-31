f275f421a8fe3fd82434ed753cd99ce306cdfc3b
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2024-01-31 08:56:36 -0300

    add account view

--- internal/models/mocks/users.go
+++ internal/models/mocks/users.go
@@ -1,6 +1,8 @@
 package mocks
 
 import (
+	"time"
+
 	"github.com/lucasgrvarela/snippetbox/internal/models"
 )
 
@@ -30,4 +32,19 @@ func (m *UserModel) Exists(id int) (bool, error) {
 	default:
 		return false, nil
 	}
+}
+
+func (m *UserModel) Get(id int) (models.User, error) {
+	if id == 1 {
+		u := models.User{
+			ID:      1,
+			Name:    "Alice",
+			Email:   "alice@example.com",
+			Created: time.Now(),
+		}
+
+		return u, nil
+	}
+
+	return models.User{}, models.ErrNoRecord
 }
